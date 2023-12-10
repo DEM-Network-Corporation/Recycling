c7a49c8ac3521bc778f6873edf268e539ffe1cb2
Arthur Yoshikasu Arakaki <ayarakaki16@gmail.com>
2023-12-10 18:48:38 -0300

    Create redirect5.js

---
+++ download/redirect5.js
@@ -0,0 +1,4 @@
+function redirect5() {
+  location.replace("https://cdn.modrinth.com/data/9L3Js3A8/versions/po75mJig/recycling_v0.0.9-alpha-mc1.20%2B1.20.1.jar")
+  window.alert("Thank You For Downloading")
+}
